22e862c7e83d86cb3229265e7ffcc3aa4e81994f
nmrlo-iscteiul <56138525+nmrlo-iscteiul@users.noreply.github.com>
2020-03-25 19:51:40 +0000

    1st

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		helloWorld();
+	}
+	
+	public static String helloWorld() {
+		String hello = "Hello world! I am alive and kicking!";
+		System.out.println(hello);
+		return hello;
+	}
+
+}
